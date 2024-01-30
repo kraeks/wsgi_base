2647996b0f38cdb5dc07efa1e7532c8ba554305c
Lars Walther <larswalther@Laptop-von-Lars.fritz.box>
2024-01-30 14:33:35 +0100

    Gegenüberstellung von 2 Implementierungsmethoden

--- 06_simple_form.py
+++ 06_simple_form.py
@@ -17,7 +17,22 @@ def application(environ, start_response):
     elif path_info == '/submit' and environ['REQUEST_METHOD'] == 'POST':
         input_data = environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', 0)))
         form_data = input_data.decode('utf-8')
+
+        #2 Methoden zur Umwandlung des form_data strings in ein Dictionary
+        # Ausführliche Methode
+        print(form_data)
+        mylist = form_data.split('&')
+        print(mylist)
+        mydict = dict()
+        for element in mylist:
+            peer = element.split("=")
+            print(peer)
+            mydict[peer[0]] = peer[1]
+        print(mydict)
+
+        # Komprimierte Methode
         user_data = dict(x.split("=") for x in form_data.split("&"))
+
         print(user_data)
 
         status = '303 See Other'
